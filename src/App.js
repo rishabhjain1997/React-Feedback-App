44e14b325e0c6552bc2583ebb1ed9843da8c216a
Rishabh Jain <rishabhjain1997@gmail.com>
2022-01-26 01:52:56 +0530

    Section 4 part 2

--- src/App.js
+++ src/App.js
@@ -1,29 +1,38 @@
-import React from "react";
-import Header from "./components/Header";
-import FeedbackList from "./components/FeedbackList";
-import { useState } from "react";
-import FeedbackData from "./data/FeedbackData";
-import FeedbackStats from "./components/FeedbackStats";
-
+import React from "react"
+import Header from "./components/Header"
+import FeedbackList from "./components/FeedbackList"
+import { useState } from "react"
+import FeedbackData from "./data/FeedbackData"
+import FeedbackStats from "./components/FeedbackStats"
+import FeedbackForm from "./components/FeedbackForm"
+import { v4 as uuidv4 } from "uuid"
 function App() {
-  const [feedback, setFeedback] = useState(FeedbackData);
+  const [feedback, setFeedback] = useState(FeedbackData)
   const deleteFeedback = (id) => {
     if (window.confirm("Are you sure you want to delete")) {
       setFeedback((feedback) => {
-        return feedback.filter((item) => item.id !== id);
-      });
+        return feedback.filter((item) => item.id !== id)
+      })
     }
-  };
-  console.log(feedback);
+  }
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4()
+
+    setFeedback((feedback) => {
+      return [...feedback, newFeedback]
+    })
+  }
+  //console.log(feedback);
   return (
     <>
       <Header text="Feedback UI" />
       <div className="container">
+        <FeedbackForm handleAdd={addFeedback} />
         <FeedbackStats feedback={feedback} />
         <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
