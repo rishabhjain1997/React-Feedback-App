ed78139432c8651a667707e4e7fc6320ec46cbce
Rishabh Jain <rishabhjain1997@gmail.com>
2022-01-28 02:27:55 +0530

    Section 5

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import { useState } from "react"
+import { v4 as uuidv4 } from "uuid"
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  NavLink,
+} from "react-router-dom"
+import Card from "./components/shared/Card"
 import React from "react"
 import Header from "./components/Header"
 import FeedbackList from "./components/FeedbackList"
-import { useState } from "react"
 import FeedbackData from "./data/FeedbackData"
 import FeedbackStats from "./components/FeedbackStats"
 import FeedbackForm from "./components/FeedbackForm"
-import { v4 as uuidv4 } from "uuid"
+import AboutPage from "./pages/AboutPage"
+import AboutIconLink from "./components/AboutIconLink"
+import Post from "./components/Post"
+
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData)
   const deleteFeedback = (id) => {
@@ -24,14 +35,40 @@ function App() {
   }
   //console.log(feedback);
   return (
-    <>
+    <Router>
       <Header text="Feedback UI" />
+      <Card>
+        <NavLink to="/" activeclassname="active">
+          Home
+        </NavLink>
+        <NavLink to="/about" activeclassname="active">
+          About
+        </NavLink>
+      </Card>
+
       <div className="container">
-        <FeedbackForm handleAdd={addFeedback} />
-        <FeedbackStats feedback={feedback} />
-        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={
+              <>
+                <FeedbackForm handleAdd={addFeedback} />
+                <FeedbackStats feedback={feedback} />
+                <FeedbackList
+                  feedback={feedback}
+                  handleDelete={deleteFeedback}
+                />
+              </>
+            }
+          ></Route>
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/post/:id/*" element={<Post />} />
+        </Routes>
+
+        <AboutIconLink />
       </div>
-    </>
+    </Router>
   )
 }
 
